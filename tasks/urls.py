d9973b3475ca09eeba1295fff01cefe2b17eeb93
Brian K. Vagnini <brian@vagnini.net>
2021-05-31 19:24:39 -0400

    Create Update functionality in app.

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name = "list"),
+    path('update_task/<str:pk>/', views.updateTask, name = "update_task") 
+    #the str:pk makes the url dynamic and makes it use the pk provides by the updateTask function pk parameter
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -8,7 +8,7 @@ def index(request):
     #return HttpResponse('sup bitches?')
     tasks = Task.objects.all()
 
-    form = TaskForm()#this is new
+    form = TaskForm()#this is new - https://docs.djangoproject.com/en/3.2/topics/forms/
     #for POST methods
     if request.method == "POST":
         form = TaskForm(request.POST)
@@ -18,3 +18,18 @@ def index(request):
 
     context = {'tasks': tasks, 'form': form} #add the form section
     return render(request, 'tasks/list.html', context)
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm (instance = task) #the instance = task refers to the task variable 2 lines above (this is how it gets the ID number)
+    #this section is new
+    if request.method == 'POST':
+        form = TaskForm (request.POST, instance = task)
+        if form.is_valid():    
+            form.save()
+        return redirect('/')
+    #end new section
+    context = {'form':form}
+
+    return render(request, 'tasks/update_task.html', context)
