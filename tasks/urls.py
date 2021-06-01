e1b9a6f27a11028a5a2ad0b01457d71fd7488af0
Brian K. Vagnini <brian@vagnini.net>
2021-05-31 22:01:22 -0400

    Add delete functionality to app.

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = "list"),
-    path('update_task/<str:pk>/', views.updateTask, name = "update_task") 
+    path('update_task/<str:pk>/', views.updateTask, name = "update_task"), 
     #the str:pk makes the url dynamic and makes it use the pk provides by the updateTask function pk parameter
+    path('deleteTask/<str:pk>/', views.deleteTask, name = "delete") 
 ]
